b981882745568aaa1a2b5daf027db0ab1471fef2
Gopal <gopalhelpyou@gmail.com>
2024-07-21 20:47:11 +0530

    code clean up again  3

--- utils/nextAuthOptions.ts
+++ utils/nextAuthOptions.ts
@@ -4,12 +4,12 @@ export const authOptions ={
     
         providers:[
           Google({
-              clientId: process.env.GOOGLE_CLIENT_ID?? "",
-          clientSecret: process.env.GOOGLE_CLIENT_SECRET ??""
+              clientId: process.env.GOOGLE_CLIENT_ID||'886971285695-ei30vgf9rfi8f5go04ppjo317l1rnto9.apps.googleusercontent.com',
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET ||"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"
       
           })
         ],
-        secret:process.env.NEXTAUTH_PASSWORD,
+        secret:process.env.NEXTAUTH_PASSWORD||'secret',
         pages:{
           signIn:'/signin'
         }
